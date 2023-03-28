8cdec68fefd1cbdf6ec8509d9889c11824cd0ac4
Muhammad Majid <127615339+Majid462@users.noreply.github.com>
2023-03-28 05:22:12 -0700

    Add files via upload

---
+++ Percentage.js
@@ -0,0 +1,6 @@
+alert("Find Your Pencentage %")
+var a =prompt("Enter Total Marks");
+var b=prompt("Enter Obtained Marks");
+var c=(b/a)*100;
+console.log("Your Percentage is "+c+"%");
+document.write("Your Percentage = "+c +"%");
